2b75f8226aabae34e9996970952cc56284a6b6f5
Jon Sullivan <JSullivan@OrangeBarrelMedia.com>
2020-08-07 15:08:17 -0400

    shore up new user signup

--- serverless/identity-signup/identity-signup.js
+++ serverless/identity-signup/identity-signup.js
@@ -1,9 +1,18 @@
 
 exports.handler = async (event, context) => {
 
-  console.log(JSON.stringify(event))
-  console.log(JSON.stringify(context))
+  // `user` doesn't actually exist yet in identity-signup
+  const { user } = context.clientContext // => null
 
+  // To get info about signup at this stage, dig into event
+  const newUser = JSON.parse(event.body).user
+  console.log(`Signed up with email ${newUser.email}`)
+
+  // Wondering if this is because clientContext is really made to hold
+  // data about an active client hitting a function directly whereas this
+  // hook is fired as a background process from the identity service. Alas.
+
+  // All new users are members :)
   return {
     statusCode: 200,
     body: JSON.stringify({
