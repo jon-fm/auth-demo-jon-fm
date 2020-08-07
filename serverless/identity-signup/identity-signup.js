20decc971b8efa71a0c4a44b2e45201081db56be
Jon Sullivan <JSullivan@OrangeBarrelMedia.com>
2020-08-07 14:53:06 -0400

    cmon logs

--- serverless/identity-signup/identity-signup.js
+++ serverless/identity-signup/identity-signup.js
@@ -1,16 +1,14 @@
 
-exports.handler = async (_, context) => {
+exports.handler = async (event, context) => {
 
-  const { user } = context.clientContext
-  user && console.log(`user.email: ${user.email}`)
-  user && console.log(`user.app_metadata: ${user.app_metadata}`)
+  console.log(JSON.stringify(event))
+  console.log(JSON.stringify(context))
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       "app_metadata": {
         roles: ["member"],
-        ...user.app_metadata
       }
     }),
   }
