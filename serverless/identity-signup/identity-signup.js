5b6293fa193e0fb4fa9f65fbd37bc43cbae760de
Jon Sullivan <JSullivan@OrangeBarrelMedia.com>
2020-08-07 14:48:29 -0400

    functions

--- serverless/identity-signup/identity-signup.js
+++ serverless/identity-signup/identity-signup.js
@@ -1,13 +1,16 @@
 
 exports.handler = async (_, context) => {
+
   const { user } = context.clientContext
-  console.log(`Signup from ${user.email}`)
+  user && console.log(`user.email: ${user.email}`)
+  user && console.log(`user.app_metadata: ${user.app_metadata}`)
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       "app_metadata": {
-        "roles": ["member"]
+        roles: ["member"],
+        ...user.app_metadata
       }
     }),
   }
